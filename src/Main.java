2737c15ee4829aac0fae1841788cf8dbaa5fd64e
CFELIPEGA623 <cfelipega@outlook.com>
2024-08-11 18:08:00 -0500

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int sumatoria = 0;
+        int i = 1;
+        while (i <= 10) {
+            sumatoria += i;
+            i++;
+        }
+        System.out.println("La sumatoria de los números del 1 al 10 es: " + sumatoria);
+    }
+}
